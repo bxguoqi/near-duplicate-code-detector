770d67f3e2e118b764891970561908f4e167f372
bxguoqi <bxguoqi@126.com>
2020-11-26 13:56:07 +0800

    mycode

--- myDup.py
+++ myDup.py
@@ -3,8 +3,10 @@ import json
 import os
 import os.path
 import gzip
+import time
 from pprint import pprint
 
+dup_mark = set()
 def get_read_gz_file_line_count(path):
     if os.path.exists(path):
         with gzip.open(path, 'rb') as f:
@@ -22,11 +24,37 @@ def read_gz_file(path):
                 yield line
     else:
         print('the path [{}] is not exist!'.format(path))
-def comp2dicts(filename,tokens):
-    ''''''
-    pass
+def comp2dicts(refers,filename,t_set,t_dict,kj,j):
+    dups = 0
+    for filename_ref,(t_set_ref,t_dict_ref) in refers.items():
+        if filename in dup_mark and filename_ref in dup_mark:
+            continue
+        # region keyJaccardSimilarity
+        t_set_u = t_set_ref.union(t_set)
+        numDistinct = len(t_set_u)
+        sameKeys = len(t_set_ref)+len(t_set)-numDistinct
+        keyJaccardSimilarity = 0 if numDistinct == 0 else float(sameKeys) / numDistinct
+        # endregion
+        if keyJaccardSimilarity<kj:
+            continue
+        numerator = 0
+        denominator = 0
+        for k in t_set_u:
+            count_ref = t_dict_ref.get(k,0)
+            count = t_dict.get(k,0)
+            numerator += min(count,count_ref)
+            denominator += max(count,count_ref)
+        jaccardSimilarity = 0 if denominator==0 else float(numerator)/denominator
+        if jaccardSimilarity<j:
+            continue
+        dup_mark.add(filename_ref)
+        dup_mark.add(filename)
+        dups+=1
+            # print("compare {0} with {1} similarity is  {2},{3}".format(filename1,filename,keyJaccardSimilarity,jaccardSimilarity))
+    return dups
 def main():
     '''
+    1974 2594
     Usage:
         python myDup.py \
          --dir=<path> \
@@ -47,9 +75,9 @@ def main():
                         help="deal with the part of the data")
     parser.add_argument("--end", default=1.0, type=float,
                         help="deal with the part of the data")
-    parser.add_argument("--key-jaccard-threshold", default=0.8,
+    parser.add_argument("--keyJaccardThreshold", default=0.8,
                         help="The Jaccard similarity threshold for token-sets [default: 0.8].")
-    parser.add_argument("--jaccard-threshold", default=0.7,
+    parser.add_argument("--jaccardThreshold", default=0.7,
                         help="The Jaccard similarity threshold for token multisets [default: 0.7].")
     args = parser.parse_args()
     # endregion
@@ -68,8 +96,9 @@ def main():
     print(size,begin,end)
 
     # compare the (begin,end] part to (begin,size] part
-    dicts = {}
+    refers = {}
     con = read_gz_file(file_path)
+    dups =0
     if getattr(con, '__iter__', None):
         for idx,line in enumerate(con):
             if idx < begin:
@@ -81,10 +110,13 @@ def main():
             for token in tokens:
                 t_dict[token] = t_dict.get(token, 0)+1
             t_set = set(t_dict.keys())
-            comp2dicts(dicts,filename,t_dict)
+            dups += comp2dicts(refers,filename,t_set,t_dict,args.keyJaccardThreshold,args.jaccardThreshold)
             if idx <=end:
-                dicts[filename] = t_dict
-
+                refers[filename] = (t_set,t_dict)
+    print(dups,len(dup_mark))
 
 if __name__ == "__main__":
+    time_start = time.time()
     main()
+    time_end = time.time()
+    print('totally cost',time_end-time_start)
