f503855d62c7a2c3d52b6249dcd5c6234e417de4
bxguoqi <bxguoqi@126.com>
2020-11-26 20:14:29 +0800

    mycode

--- myDup.py
+++ myDup.py
@@ -4,9 +4,11 @@ import os
 import os.path
 import gzip
 import time
-from pprint import pprint
 
-dup_mark = set()
+dups = 0
+labeb_set = set()
+dup_mark = {}
+
 def get_read_gz_file_line_count(path):
     if os.path.exists(path):
         with gzip.open(path, 'rb') as f:
@@ -24,10 +26,27 @@ def read_gz_file(path):
                 yield line
     else:
         print('the path [{}] is not exist!'.format(path))
+def load_data(filename):
+    global dups
+    global labeb_set
+    global dup_mark
+    if not os.path.exists(filename):
+        dups = 0
+        labeb_set = set()
+        dup_mark = {}
+    else:
+        with open(filename) as f:
+            dups = json.loads(f.readline())
+            label_list = json.loads(f.readline())
+            labeb_set = set(label_list)
+            dup_mark = json.loads(f.readline())
+
+
 def comp2dicts(refers,filename,t_set,t_dict,kj,j):
-    dups = 0
+    # refer 里的id较小，使用归并集，把大的类别归并到小的
+    dups_local = 0
     for filename_ref,(t_set_ref,t_dict_ref) in refers.items():
-        if filename in dup_mark and filename_ref in dup_mark:
+        if filename in dup_mark.keys() and filename_ref in dup_mark.keys():
             continue
         # region keyJaccardSimilarity
         t_set_u = t_set_ref.union(t_set)
@@ -47,29 +66,34 @@ def comp2dicts(refers,filename,t_set,t_dict,kj,j):
         jaccardSimilarity = 0 if denominator==0 else float(numerator)/denominator
         if jaccardSimilarity<j:
             continue
-        dup_mark.add(filename_ref)
-        dup_mark.add(filename)
-        dups+=1
-            # print("compare {0} with {1} similarity is  {2},{3}".format(filename1,filename,keyJaccardSimilarity,jaccardSimilarity))
-    return dups
+        label = dup_mark.get(filename_ref,filename_ref)
+        labeb_set.add(label)
+        dup_mark[filename_ref] = label
+        dup_mark[filename] = label
+        dups_local+=1
+        print("compare {0} with {1} similarity is  {2},{3}".format(filename_ref,filename,keyJaccardSimilarity,jaccardSimilarity))
+    return dups_local
 def main():
     '''
-    1974 2594
     Usage:
         python myDup.py \
          --dir=<path> \
          --key-jaccard-threshold=<val> \
          --jaccard-threshold=<val>
-         eg:python myDup.py --dir tokens --begin 0.1 --end 0.3
+         myDup.py --dir tokens --result result.json --begin 0 --end 0.1
         Options:
           --dir=<path>                   Directory where .jsonl.gz files live.
           --key-jaccard-threshold=<val>  The Jaccard similarity threshold for token-sets [default: 0.8].
           --jaccard-threshold=<val>      The Jaccard similarity threshold for token multisets [default: 0.7].
     '''
-
-    # region parameters
+    global dups
+    global labeb_set
+    global dup_mark
+    # region get parameters
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", default=None, type=str, required=True,
+                        help="Directory where .jsonl.gz files live.")
+    parser.add_argument("--result", default=None, type=str, required=True,
                         help="Directory where .jsonl.gz files live.")
     parser.add_argument("--begin", default=0.0, type=float,
                         help="deal with the part of the data")
@@ -81,6 +105,9 @@ def main():
                         help="The Jaccard similarity threshold for token multisets [default: 0.7].")
     args = parser.parse_args()
     # endregion
+
+    load_data(args.result)
+    print("duplicate files:{}, in {} clusters".format(dups, len(labeb_set)))
 
     file_path=''
     for root, dirs, files in os.walk(args.dir):
@@ -98,7 +125,6 @@ def main():
     # compare the (begin,end] part to (begin,size] part
     refers = {}
     con = read_gz_file(file_path)
-    dups =0
     if getattr(con, '__iter__', None):
         for idx,line in enumerate(con):
             if idx < begin:
@@ -113,8 +139,19 @@ def main():
             dups += comp2dicts(refers,filename,t_set,t_dict,args.keyJaccardThreshold,args.jaccardThreshold)
             if idx <=end:
                 refers[filename] = (t_set,t_dict)
-    print(dups,len(dup_mark))
+    print("duplicate files:{}, in {} clusters".format(dups,len(labeb_set)))
+    #save dups,label_set and dup_mark
+    with open(args.result, 'w') as f:
+        json.dump(dups,f)
+        f.write('\n')
+        json.dump(list(labeb_set),f)
+        f.write('\n')
+        json.dump(dup_mark,f)
 
+
+
+
+#duplicate files:1974, in 681 clusters
 if __name__ == "__main__":
     time_start = time.time()
     main()
